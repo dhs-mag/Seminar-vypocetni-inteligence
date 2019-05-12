6d72b3b046e1e2bf25fef2ae5295520b322c3c0d
Frank Mastil <fr.mastil@gmail.com>
2019-05-12 21:49:45 +0200

    iris cleaning

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import random as random
 
-def sigmoida(phi):
-    return np.round(1.0 / (1.0 + np.exp(-phi)), 15)
+
+def sigmoid(x):
+    return np.round(1.0 / (1.0 + np.exp(-x)), 15)
 
 
 alpha = 0.5  # rychlost učení
 eta = 0.1  # setrvačnost
 
 
-class Percepton:
+class Perceptron:
     def __init__(self, num_outputs, num_inputs, activation):
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
-        self.w = np.zeros((num_outputs, num_inputs))
+        self.weight = np.zeros((num_outputs, num_inputs))
         self.th = np.zeros(num_outputs)
         self.activation_function = activation
         self.delta = np.zeros(num_outputs)
@@ -36,7 +37,7 @@ class Percepton:
         self.dths += -self.delta
 
     def back_propagate(self, prevLayer):
-        prevLayer.delta = (np.transpose(self.w) @ self.delta) * (prevLayer.outputs * (1 - prevLayer.outputs))
+        prevLayer.delta = (np.transpose(self.weight) @ self.delta) * (prevLayer.outputs * (1 - prevLayer.outputs))
 
     def epoch_start(self):
         self.dws = np.zeros((self.num_outputs, self.num_inputs))
@@ -45,7 +46,7 @@ class Percepton:
 
     def epoch_finish(self):
         dws_temp = alpha * self.dws + eta * self.odw
-        self.w += dws_temp
+        self.weight += dws_temp
         self.odw = dws_temp
 
         dths_temp = alpha * self.dths + eta * self.odth
@@ -53,13 +54,13 @@ class Percepton:
         self.odth = dths_temp
 
     def recall(self, inputs_array):
-        self.outputs = self.activation_function(self.w @ inputs_array - self.th)
+        self.outputs = self.activation_function(self.weight @ inputs_array - self.th)
         return self.outputs
 
     def init(self, random_range_min, random_range_max):
-        for x in range(len(self.w)):
-            for y in range(len(self.w[x])):
-                self.w[x][y] = random.uniform(random_range_max, random_range_min)
+        for x in range(len(self.weight)):
+            for y in range(len(self.weight[x])):
+                self.weight[x][y] = random.uniform(random_range_max, random_range_min)
 
         for x in range(len(self.th)):
             self.th[x] = random.uniform(random_range_max, random_range_min)
@@ -89,75 +90,83 @@ class Percepton:
 
 
 class Net:
-    def __init__(self):
-        self.layers = []
-        self.output = []
+    def __init__(self, layer=None):
+        if layer is None:
+            layer = []
+
+        self.hidden_layer = layer
+        # self.output = []
 
     def recall(self, x):
-        self.layers[0].recall(x)
-        return self.layers[1].recall(self.layers[0].outputs)
+        self.hidden_layer[0].recall(x)
+        return self.hidden_layer[1].recall(self.hidden_layer[0].outputs)
+
+    def add_neuron(self, neuron: Perceptron):
+        self.hidden_layer.append(neuron)
 
     def net_init(self, random_range_min, random_range_max):
-        self.layers = []
-        self.layers.append(Percepton(4, 4, sigmoida))
-        self.layers.append(Percepton(3, 4, sigmoida))
-        self.output = self.layers[1]
-        for l in self.layers:
+        # self.hidden_layer = []
+        # self.output = self.hidden_layer[1]
+        for l in self.hidden_layer:
             l.init(random_range_min, random_range_max)
 
     def epoch_start(self):
-        for l in self.layers:
+        for l in self.hidden_layer:
             l.epoch_start()
 
     def epoch_finish(self):
-        for l in self.layers:
+        for l in self.hidden_layer:
             l.epoch_finish()
 
     def learn(self, x, d):
         self.recall(x)
-        e = self.layers[1].output_delta(d)
-        self.layers[1].learn(self.layers[0].outputs)
-        self.layers[1].back_propagate(self.layers[0])
-        self.layers[0].learn(x)
+        e = self.hidden_layer[1].output_delta(d)
+        self.hidden_layer[1].learn(self.hidden_layer[0].outputs)
+        self.hidden_layer[1].back_propagate(self.hidden_layer[0])
+        self.hidden_layer[0].learn(x)
         return e
 
     def print_net(self):
-        print("% 1.15f" % self.layers[1].outputs[0] + "; output: y")
-        print("% 1.15f" % self.layers[1].th[0] + "; output: threshold")
-        print("% 1.15f" % self.layers[1].w[0][0] + "; output: w[0]")
-        print("% 1.15f" % self.layers[1].w[0][1] + "; output: w[1]")
-        print("% 1.15f" % self.layers[1].delta[0] + "; output: delta")
-        print("% 1.15f" % self.layers[1].dths[0] + "; output: delta threshold")
-        print("% 1.15f" % self.layers[1].dws[0][0] + "; output: delta w[0]")
-        print("% 1.15f" % self.layers[1].dws[0][1] + "; output: delta w[1]")
-        print("% 1.15f" % self.layers[0].outputs[0] + "; hidden: y[0]")
-        print("% 1.15f" % self.layers[0].outputs[1] + "; hidden: y[1]")
-        print("% 1.15f" % self.layers[0].th[0] + "; hidden: threshold[0]")
-        print("% 1.15f" % self.layers[0].th[1] + "; hidden: threshold[1]")
-        print("% 1.15f" % self.layers[0].w[0][0] + "; hidden: w[0][0]")
-        print("% 1.15f" % self.layers[0].w[0][1] + "; hidden: w[0][1]")
-        print("% 1.15f" % self.layers[0].w[1][0] + "; hidden: w[1][0]")
-        print("% 1.15f" % self.layers[0].w[1][1] + "; hidden: w[1][1]")
-        print("% 1.15f" % self.layers[0].delta[0] + "; hidden: delta[0]")
-        print("% 1.15f" % self.layers[0].delta[1] + "; hidden: delta[1]")
-        print("% 1.15f" % self.layers[0].dths[0] + "; hidden: delta hreshold[0]")
-        print("% 1.15f" % self.layers[0].dths[1] + "; hidden: delta threshold[1]")
-        print("% 1.15f" % self.layers[0].dws[0][0] + "; hidden: delta w[0][0]")
-        print("% 1.15f" % self.layers[0].dws[0][1] + "; hidden: delta w[0][1]")
-        print("% 1.15f" % self.layers[0].dws[1][0] + "; hidden: delta w[1][0]")
-        print("% 1.15f" % self.layers[0].dws[1][1] + "; hidden: delta w[1][1]")
-        print("% 1.15f" % self.layers[0].odw[0][0] + "; hidden: old delta w[0][0]")
-        print("% 1.15f" % self.layers[0].odw[0][1] + "; hidden: old delta w[0][1]")
-        print("% 1.15f" % self.layers[0].odw[1][0] + "; hidden: old delta w[1][0]")
-        print("% 1.15f" % self.layers[0].odw[1][1] + "; hidden: old delta w[1][1]")
+        print("% 1.15f" % self.hidden_layer[1].outputs[0] + "; output: y")
+        print("% 1.15f" % self.hidden_layer[1].th[0] + "; output: threshold")
+        print("% 1.15f" % self.hidden_layer[1].w[0][0] + "; output: w[0]")
+        print("% 1.15f" % self.hidden_layer[1].w[0][1] + "; output: w[1]")
+        print("% 1.15f" % self.hidden_layer[1].delta[0] + "; output: delta")
+        print("% 1.15f" % self.hidden_layer[1].dths[0] + "; output: delta threshold")
+        print("% 1.15f" % self.hidden_layer[1].dws[0][0] + "; output: delta w[0]")
+        print("% 1.15f" % self.hidden_layer[1].dws[0][1] + "; output: delta w[1]")
+        print("% 1.15f" % self.hidden_layer[0].outputs[0] + "; hidden: y[0]")
+        print("% 1.15f" % self.hidden_layer[0].outputs[1] + "; hidden: y[1]")
+        print("% 1.15f" % self.hidden_layer[0].th[0] + "; hidden: threshold[0]")
+        print("% 1.15f" % self.hidden_layer[0].th[1] + "; hidden: threshold[1]")
+        print("% 1.15f" % self.hidden_layer[0].w[0][0] + "; hidden: w[0][0]")
+        print("% 1.15f" % self.hidden_layer[0].w[0][1] + "; hidden: w[0][1]")
+        print("% 1.15f" % self.hidden_layer[0].w[1][0] + "; hidden: w[1][0]")
+        print("% 1.15f" % self.hidden_layer[0].w[1][1] + "; hidden: w[1][1]")
+        print("% 1.15f" % self.hidden_layer[0].delta[0] + "; hidden: delta[0]")
+        print("% 1.15f" % self.hidden_layer[0].delta[1] + "; hidden: delta[1]")
+        print("% 1.15f" % self.hidden_layer[0].dths[0] + "; hidden: delta hreshold[0]")
+        print("% 1.15f" % self.hidden_layer[0].dths[1] + "; hidden: delta threshold[1]")
+        print("% 1.15f" % self.hidden_layer[0].dws[0][0] + "; hidden: delta w[0][0]")
+        print("% 1.15f" % self.hidden_layer[0].dws[0][1] + "; hidden: delta w[0][1]")
+        print("% 1.15f" % self.hidden_layer[0].dws[1][0] + "; hidden: delta w[1][0]")
+        print("% 1.15f" % self.hidden_layer[0].dws[1][1] + "; hidden: delta w[1][1]")
+        print("% 1.15f" % self.hidden_layer[0].odw[0][0] + "; hidden: old delta w[0][0]")
+        print("% 1.15f" % self.hidden_layer[0].odw[0][1] + "; hidden: old delta w[0][1]")
+        print("% 1.15f" % self.hidden_layer[0].odw[1][0] + "; hidden: old delta w[1][0]")
+        print("% 1.15f" % self.hidden_layer[0].odw[1][1] + "; hidden: old delta w[1][1]")
 
 
 def normalize(value, min, max):
     return (value - min) / (max - min)
 
 
-if __name__ == "__main__":
+def main():
     net = Net()
+
+    net.add_neuron(Perceptron(4, 4, sigmoid))
+    net.add_neuron(Perceptron(3, 4, sigmoid))
+
     net.net_init(-0.3, 0.3)
 
     filename = 'iris.csv'
@@ -165,11 +174,11 @@ if __name__ == "__main__":
     reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
     x = list(reader)
 
-    trainSet = []
+    train_set = []
 
     for item in x:
         if len(item) > 0:
-            trainSet.append(
+            train_set.append(
                 [
                     np.array([
                         normalize(float(item[0]), 4.3, 7.9),
@@ -184,42 +193,39 @@ if __name__ == "__main__":
                     ])
                 ])
 
-    rms = []
+    errors = []
     epoch = []
-    avgErr = 0
-    err = 0
 
     for i in range(10000):
-        avgErr = 0
+        avg_error = 0
         net.epoch_start()
-        for pat in trainSet:
-            avgErr += net.learn(pat[0], pat[1])
+        for pat in train_set:
+            avg_error += net.learn(pat[0], pat[1])
         net.epoch_finish()
-        err = avgErr / len(trainSet)
-
-        if err < 0.009:
-            print("EPOCH:", i + 1)
-            print("Error:", err)
-            print("----------------------------")
-            break
-
-        rms.append(err)
+        error = avg_error / len(train_set)
+        errors.append(error)
         epoch.append(i + 1)
 
         if i % 100 == 0:
             print("EPOCH:", i + 1)
-            print("Error:", err)
+            print("Error:", error)
             print("----------------------------")
+            if error < 0.009:
+                break
 
     # graph
-    fig, axs = plt.subplots(1, 1)
-    axs.plot(epoch, rms)
-    axs.set_xlabel('Epoch')
-    axs.set_ylabel('Error')
-    axs.grid(True)
-    plt.show()
+    figure, axis = plot.subplots(1, 1)
+    axis.plot(epoch, errors)
+    axis.set_xlabel('Epoch')
+    axis.set_ylabel('Error')
+    axis.grid(True)
+    plot.show()
 
-    print("After learn " + str(trainSet[0][1]) + " :", np.round(net.recall(trainSet[0][0])))
-    print("After learn " + str(trainSet[55][1]) + " :", np.round(net.recall(trainSet[55][0])))
-    print("After learn " + str(trainSet[107][1]) + " :", np.round(net.recall(trainSet[107][0])))
-    print("After learn " + str(trainSet[142][1]) + " :", np.round(net.recall(trainSet[142][0])))
+    print("After learn " + str(train_set[0][1]) + " :", np.round(net.recall(train_set[0][0])))
+    print("After learn " + str(train_set[55][1]) + " :", np.round(net.recall(train_set[55][0])))
+    print("After learn " + str(train_set[107][1]) + " :", np.round(net.recall(train_set[107][0])))
+    print("After learn " + str(train_set[142][1]) + " :", np.round(net.recall(train_set[142][0])))
+
+
+if __name__ == "__main__":
+    main()
