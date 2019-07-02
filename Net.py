3877aad32c4549773cf78f30270384e5a8268d88
Ondrej Doktor <doktor.ml@gmail.com>
2019-07-02 12:40:51 +0200

    pokus o cisteni

--- Net.py
+++ Net.py
@@ -19,23 +19,6 @@ class Net:
         self.output = self.layers[1]
         for l in self.layers:
             l.init(randon_range_min, randon_range_max)
-        # self.layers[0].w[0][0] = -  0.214767760000000
-        # self.layers[0].w[0][1] = -  0.045404790000000
-        # self.layers[0].w[1][0] =    0.106739550000000
-        # self.layers[0].w[1][1] =    0.136999780000000
-        # self.layers[0].th[0]   = -  0.299236760000000
-        # self.layers[0].th[1]   =    0.122603690000000
-        # self.layers[1].w[0][0] =    0.025870070000000
-        # self.layers[1].w[0][1] =    0.168638190000000
-        # self.layers[1].th[0]   =    0.019322390000000
-
-    def epochStart(self):
-        for l in self.layers:
-            l.epochStart()
-
-    def epochFinish(self):
-        for l in self.layers:
-            l.epochFinish()
 
     def learn(self, x, d):
         self.recall(x)
@@ -45,35 +28,3 @@ class Net:
         self.layers[1].backPropagate(self.layers[0])
         self.layers[0].learn(x)
         return e
-
-    def print_net(self, file=sys.stdout):
-        print(("  % 1.15f" % self.layers[1].outputs[0]).replace('.', ',') + " ; output: y ", file=file)
-        print(("  % 1.15f" % -self.layers[1].th[0]).replace('.', ',') + " ; output: threshold", file=file)
-        print(("  % 1.15f" % self.layers[1].w[0][0]).replace('.', ',') + " ; output: w[0]", file=file)
-        print(("  % 1.15f" % self.layers[1].w[0][1]).replace('.', ',') + " ; output: w[1]", file=file)
-        print(("  % 1.15f" % self.layers[1].delta[0]).replace('.', ',') + " ; output: delta", file=file)
-        print(("  % 1.15f" % -self.layers[1].dths[0]).replace('.', ',') + " ; output: delta threshold", file=file)
-        print(("  % 1.15f" % self.layers[1].dws[0][0]).replace('.', ',') + " ; output: delta w[0]", file=file)
-        print(("  % 1.15f" % self.layers[1].dws[0][1]).replace('.', ',') + " ; output: delta w[1]", file=file)
-        print(("  % 1.15f" % self.layers[0].outputs[0]).replace('.', ',') + " ; hidden: y[0]", file=file)
-        print(("  % 1.15f" % self.layers[0].outputs[1]).replace('.', ',') + " ; hidden: y[1]", file=file)
-        print(("  % 1.15f" % -self.layers[0].th[0]).replace('.', ',') + " ; hidden: threshold [0]", file=file)
-        print(("  % 1.15f" % -self.layers[0].th[1]).replace('.', ',') + " ; hidden: threshold [1]", file=file)
-        print(("  % 1.15f" % self.layers[0].w[0][0]).replace('.', ',') + " ; hidden: w[0][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].w[0][1]).replace('.', ',') + " ; hidden: w[0][1]", file=file)
-        print(("  % 1.15f" % self.layers[0].w[1][0]).replace('.', ',') + " ; hidden: w[1][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].w[1][1]).replace('.', ',') + " ; hidden: w[1][1]", file=file)
-        print(("  % 1.15f" % self.layers[0].delta[0]).replace('.', ',') + " ; hidden: delta [0]", file=file)
-        print(("  % 1.15f" % self.layers[0].delta[1]).replace('.', ',') + " ; hidden: delta [1]", file=file)
-        print(("  % 1.15f" % -self.layers[0].dths[0]).replace('.', ',') + " ; hidden: delta threshold [0]", file=file)
-        print(("  % 1.15f" % -self.layers[0].dths[1]).replace('.', ',') + " ; hidden: delta threshold [1]", file=file)
-        print(("  % 1.15f" % self.layers[0].dws[0][0]).replace('.', ',') + " ; hidden: delta w[0][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].dws[0][1]).replace('.', ',') + " ; hidden: delta w[0][1]", file=file)
-        print(("  % 1.15f" % self.layers[0].dws[1][0]).replace('.', ',') + " ; hidden: delta w[1][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].dws[1][1]).replace('.', ',') + " ; hidden: delta w[1][1]", file=file)
-        print(("  % 1.15f" % -self.layers[0].odth[0]).replace('.', ',') + " ; hidden: old delta threshold [0]", file=file)
-        print(("  % 1.15f" % -self.layers[0].odth[1]).replace('.', ',') + " ; hidden: old delta threshold [1]", file=file)
-        print(("  % 1.15f" % self.layers[0].odw[0][0]).replace('.', ',') + " ; hidden: old delta w[0][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].odw[0][1]).replace('.', ',') + " ; hidden: old delta w[0][1]", file=file)
-        print(("  % 1.15f" % self.layers[0].odw[1][0]).replace('.', ',') + " ; hidden: old delta w[1][0]", file=file)
-        print(("  % 1.15f" % self.layers[0].odw[1][1]).replace('.', ',') + " ; hidden: old delta w[1][1]", file=file)
--- main.py
+++ main.py
@@ -21,13 +21,6 @@ if __name__ == "__main__":
 
     trainSet = []
 
-    # trainSet = np.array([
-    #     np.array([np.array([0, 0]), np.array([0])]),
-    #     np.array([np.array([1, 0]), np.array([1])]),
-    #     np.array([np.array([0, 1]), np.array([1])]),
-    #     np.array([np.array([1, 1]), np.array([0])]),
-    # ])
-
     for patternId in range(0, 20):
 
         loadedImage = loadImageToArray("images/"+str(patternId).rjust(2, '0')+".png")
@@ -51,15 +44,12 @@ if __name__ == "__main__":
 
     outfile = open('output/code_images.txt', 'w')
 
-    net.print_net(outfile)
-
     avgErr = 0
     err = 0
-    for i in range(24):
+    for i in range(1000):
         print("# epocha", i+1, file=outfile)
 
         avgErr = 0
-        net.epochStart()
         iteration = 1
         for pat in trainSet:
             print("# iterace", iteration, file=outfile)
@@ -67,24 +57,24 @@ if __name__ == "__main__":
 
             print(("  % 1.15f" % pat[0][0]).replace('.', ',') + " ; input [0]", file=outfile)
             print(("  % 1.15f" % pat[0][1]).replace('.', ',') + " ; input [1]", file=outfile)
-            net.print_net(outfile)
 
             iteration += 1
-        net.epochFinish()
         err = avgErr/len(trainSet)
 
         print("# weight update", file=outfile)
         print(("  % 1.15f" % err).replace('.', ',') + " ; mse", file=outfile)
-        net.print_net(outfile)
 
-        if err < 0.05:
-            break
-        # print("Error:", err)
+        # if err < 0.05:
+        #     break
+        print("Error:", err)
         # print("========================")
 
-    net.print_net()
+    for patternId in range(0, 20):
+        print("After learn "+str(patternId)+": ")
 
-    print("After learn 0,0:", net.recall(trainSet[0][0]))
-    print("After learn 1,0:", net.recall(trainSet[1][0]))
-    print("After learn 0,1:", net.recall(trainSet[2][0]))
-    print("After learn 1,1:", net.recall(trainSet[3][0]))
+        output = net.recall(trainSet[0][0])
+
+        print("[", end='')
+        for x in output:
+            print(("%1.15f, " % x), end='')
+        print("]")
